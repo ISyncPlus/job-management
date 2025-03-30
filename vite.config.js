bad43f4251f9fed67ace64cac3d068acdc1558b0
Ebube Ezedimbu <eezedimbu@gmail.com>
2025-03-30 07:32:51 +0100

    vite again..

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,33 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-
-export default defineConfig({
-  base: '/', 
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  root: '.', 
-  publicDir: 'public',
-  build: {
-    outDir: 'dist',
-  },
-  esbuild: {
-    loader: "jsx",
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL || "https://mock-jobs-back.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    base: '/', 
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    root: '.', 
+    publicDir: 'public',
+    build: {
+      outDir: 'dist',
+    },
+    esbuild: {
+      loader: "jsx",
+    },
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || "https://mock-jobs-back.onrender.com",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  };
 });
